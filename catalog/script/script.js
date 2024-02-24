d6aba86c671c8770c9646546148845ae166d9694
Darua Titova <dachulatitova@gmail.com>
2024-02-24 23:20:18 +0300

    basket

--- catalog/script/script.js
+++ catalog/script/script.js
@@ -1,132 +1,16 @@
-// const personal = document.getElementsByClassName('personal')[0];
-// const popup = document.getElementsByClassName('popup')[0];
-// const btn_x = document.getElementsByClassName('btn_x')[0];
-// const bg = document.getElementsByClassName('popup_body')[0];
-// const body = document.body;
-// const telInput = document.querySelector('.tel_input');
-// const phoneBtn = document.querySelector('.personal_cab_btn');
-// const secPopup = document.querySelector('.modal_pin_code');
-// let phoneNumber = '';
-// const pinCode_btnX = document.querySelector('.pin_code_x')
-// const telChange = document.querySelector('.tel_mask')
-// const returnLink = document.querySelector('.no_code')
-// const time = document.querySelector('.time')
-// const bgCode = document.querySelector('.pin_code_body')
-// //poppup
-// document.body.style.overflowY = 'scroll';
-// personal.addEventListener('click', {
-//   handleEvent(event) {
+"use strict"
 
-//     popup.classList.add('open');
-//     document.body.style.overflowY = 'hidden';
-//   }
-// });
-
-// btn_x.addEventListener('click', {
-//   handleEvent(event) {
-//     popup.classList.remove('open');
-//     document.body.style.overflowY = 'scroll';
-//   }
-// });
-
-// bg.addEventListener('click', function (e) {
-//   if (e.target === bg) {
-//     popup.classList.remove('open');
-//     document.body.style.overflowY = 'scroll';
-//   }
-// });
-
-// //TEL MASK
-// const mask = IMask(telInput, {
-//   mask: '{+7}(000)-000-00-00'
-// });
-
-// telInput.addEventListener('input', function() {
-//   console.log('wk')
-//   if (mask.masked.isComplete) {
-//     phoneBtn.classList.add('active');
-    
-//   } else {
-//     phoneBtn.classList.remove('active');
-//   }
-// });
-
-// //LOGIN SMS  phoneBtn
-// phoneBtn.addEventListener('click', function (e) {
-//   console.log('sksk');
-//   popup.classList.remove('open');
-//   secPopup.classList.add('open');
-
-//   console.log(mask.masked.value);
-//   telChange.innerHTML = mask.masked.value;
-
-
-// });
-
-
-// pinCode_btnX.addEventListener('click', {
-//   handleEvent(event) {
-//     secPopup.classList.remove('open');
-//     document.body.style.overflowY = 'scroll';
-//   }
-// });
-// bg.addEventListener('click', function (e) {
-//   if (e.target === bg) {
-//     popup.classList.remove('open');
-//     document.body.style.overflowY = 'scroll';
-//   }
-// });
-
-// returnLink.addEventListener('click', function (e) {
-//   secPopup.classList.remove('open');
-//   popup.classList.add('open');
-
-// })
-
-// bgCode.addEventListener('click', function (e) {
-//   if (e.target === bgCode) {
-//     secPopup.classList.remove('open');
-//     document.body.style.overflowY = 'scroll';
-//   }
-// });
-
-
-// const burgerMenu = document.querySelector(".burger");
-// const burgerPopup = document.querySelector(".popup-burger");
-// let burgerStatus = true;
-// const categories = document.querySelector(".burger-categories");
-// const brends = document.querySelector(".burger-brends");
-// const headerCategories = document.querySelector(".header_categories");
-// const headerBrends = document.querySelector(".header_brends")
-
-// burgerMenu.addEventListener("click", function () {
-//     burgerPopup.classList.toggle("active");
-//     categories.classList.add("active");
-//     brends.classList.remove("active");
-//     headerCategories.classList.add("active");
-//     headerBrends.classList.remove("active");
-// })
-
-// headerCategories.addEventListener("click", function () {
-//     categories.classList.add("active");
-//     brends.classList.remove("active");
-//     headerCategories.classList.add("active");
-//     headerBrends.classList.remove("active");
-// })
-
-// headerBrends.addEventListener("click", function () {
-//     categories.classList.remove("active");
-//     brends.classList.add("active");
-//     headerCategories.classList.remove("active");
-//     headerBrends.classList.add("active");
-// })
+import { basketIncludes,
+    setBasketLocalStorage,
+    getBasketLocalStorage
+ } from "../script/utils.js";
 
 let productsData = []
 let showBtn = document.querySelector('.show_more');
 const products = document.querySelector('.products');
 let shownCards = 4;
 let countCreateCards = 1;
-const cardCount = 3;
+const cardCount = 4;
 
 
 
@@ -166,7 +50,7 @@ function createCards(data) {
         console.log(card)
 		const cardItem = 
 			`
-            <div className="card" data-product-id="${id}">
+            <div class="card" className="card" data-id="${id}">
             <div class="header_card">
                 <div class="card_status">${status}</div>
                 <div class="like_status"></div>
@@ -181,6 +65,7 @@ function createCards(data) {
                 <div class="discount">${sale}</div>
                 </div>
             </div>
+            <div class="put_bg"><button class="put">Добавить в корзину</button></div>
             </div>
             `
         products.insertAdjacentHTML('beforeEnd', cardItem);
@@ -195,7 +80,7 @@ function sliceArrCards() {
 
     const countShowCards = cardCount * countCreateCards;
 
-    const arrCards = productsData.slice(shownCards-1, countShowCards);
+    const arrCards = productsData.slice(shownCards, countShowCards);
     createCards(arrCards);
 
     shownCards = products.children.length;
@@ -206,4 +91,39 @@ function sliceArrCards() {
 }
 
 showBtn.addEventListener('click', sliceArrCards);
+products.addEventListener('click', handleCardClick);
 
+
+
+
+function handleCardClick(event) {
+    const targetBtn = event.target.closest('.put');
+    // console.log(targetBtn)
+    
+    // if (!targetBtn) return;
+
+    const card = document.querySelector('.card');
+    const id = card.dataset.id;
+    const basket = getBasketLocalStorage()
+
+    basket.push(id);
+
+    setBasketLocalStorage(basket)
+    chekingActiveBtn(basket);
+}
+
+
+function chekingActiveBtn(basket) {
+    const btn = document.querySelector('.card');
+    console.log(btn.forEach())
+
+    btn.forEach(button());
+
+    function button() {
+            const card = button.closest('.card');
+            const id = card.dataset.id;
+            const isInBasket = basket.includes(id);
+    
+            button.textContent = isInBasket ? 'В корзине' : 'Добавить в корзину';
+    }
+}
--- catalog/script/utils.js
+++ catalog/script/utils.js
@@ -5,22 +5,29 @@ export function getBasketLocalStorage() {
 
 // Запись id товаров в LS
 export function setBasketLocalStorage(basket) {
-    const basketCount = document.querySelector('.basket__count');
+    const basketCount = document.querySelector('.bascket_num');
     localStorage.setItem('basket', JSON.stringify(basket));
     basketCount.textContent = basket.length;
+    console.log(basket)
+}
+
+export function basketIncludes(basket) {
+    localStorage.setItem('basket', JSON.stringify(basket));
+
+    console.log(localStorage.basket)
 }
 
 // Проверка, существует ли товар указанный в LS 
 //(если например пару дней не заходил юзер, а товар, который у него в корзине, уже не существует)
-export function checkingRelevanceValueBasket(productsData) {
-    const basket = getBasketLocalStorage();
+// export function checkingRelevanceValueBasket(productsData) {
+//     const basket = getBasketLocalStorage();
 
-    basket.forEach((basketId, index) => {
-        const existsInProducts = productsData.some(item => item.id === Number(basketId));
-        if (!existsInProducts) {
-            basket.splice(index, 1);
-        }
-    });
+//     basket.forEach((basketId, index) => {
+//         const existsInProducts = productsData.some(item => item.id === Number(basketId));
+//         if (!existsInProducts) {
+//             basket.splice(index, 1);
+//         }
+//     });
 
-    setBasketLocalStorage(basket);
-}
+//     setBasketLocalStorage(basket);
+// }
